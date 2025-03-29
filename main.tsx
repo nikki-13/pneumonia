78de7c46f271aba7728f19ed043df07904818ed0
nikki-13 <somanikhilsai13@gmail.com>
2025-03-30 00:33:10 +0530

    .

--- main.tsx
+++ main.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter> {/* Changed from BrowserRouter to HashRouter */}
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
